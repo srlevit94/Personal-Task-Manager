a9b5d63ae6810e4becc604eceb5130caba6a0f99
Vulnus <consilim.vulnus@gmail.com>
2023-01-26 09:14:32 -0800

    Edited server.js code. Handle bars/node working on test

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs.create({ defaultLayout: 'main' }).engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('main', {title: 'Main'});
+    res.render('homepage', {title: 'Home'});
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
