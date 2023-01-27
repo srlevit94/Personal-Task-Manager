8f11f97714fcc6a94b79760a3c87491bad4ab733
Madison <thefourthb@gmail.com>
2023-01-26 21:25:31 -0800

    lavinia-models

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./User');
+const Project = require('./Project');
+
+User.hasMany(Project, {
+  foreignKey: 'project_id',
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Project };
