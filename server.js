02b2c3471c1a4848c737aebcac9705c9be62a681
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-01 16:26:17 -0700

    handlebars update: routing establishe and edits to main.handlebars html

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var app = express();
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs.create({ defaultLayout: 'main' }).engine);
-app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('homepage', {title: 'Home'});
+app.engine('handlebars', exphbs.engine({extname: 'handlebars', defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
+
+
+// routing
+// "/" template currently set to index.handlebars for personal testing
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.get('/homepage', (req, res) => {
+    res.render('homepage');
 });
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server is starting at port ', 3000);
+});
+
+
+
 
