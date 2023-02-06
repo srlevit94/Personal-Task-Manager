3f21e8c7b350974bc9239b1642f832f92239e471
Caleb Lockwood <caleblockwood@iMac.local>
2023-02-05 17:23:17 -0700

    menu bar fixes, readibility fixes, tailwind ect

--- public/js/chart.js
+++ public/js/chart.js
@@ -7,7 +7,7 @@ window.onload = function () {
                     let myChart = document.getElementById('myChart').getContext('2d');
 
                     let barChart = new Chart(myChart, {
-                        type: 'bar', data: { labels: arr.name, datasets: [{ label: 'Time Spent  (MINUTES)', data: arr.time, backgroundColor: 'lightblue' }] }, options: {
+                        type: 'bar', data: { labels: arr.name, datasets: [{ label: 'Time Spent  (MINUTES)', data: arr.time, backgroundColor: 'orange' }] }, options: {
                             responsive:
                                 true, maintainAspectRatio: false,
                         }
